c0f18f6da337fe8765ae1d33d2cd16ef7c47e7b5
unknown <2004neetupal@gmail.com>
2024-11-13 22:32:36 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { createContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes,Route } from 'react-router-dom'
+
+import { Routes, Route } from 'react-router-dom'
 
 import { SideBar } from './component/Sidebar'
 
